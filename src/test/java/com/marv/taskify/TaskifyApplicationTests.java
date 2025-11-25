3b8d3b8f7c877f0a6c4a434f6b6e5f61ca16fc53
MarvsConcept <Marvade8@gmail.com>
2025-11-25 13:40:55 -0800

    Initial Commit

---
+++ src/test/java/com/marv/taskify/TaskifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marv.taskify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
